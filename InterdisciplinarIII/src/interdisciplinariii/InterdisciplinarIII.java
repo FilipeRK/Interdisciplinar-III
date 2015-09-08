d02cc6d1743a2a467fa5653a5f0ddcefc4910aad
rr0107355 <rafaelradunz1@gmail.com>
2015-09-08 09:45:30 -0300

    Penes

---
+++ InterdisciplinarIII/src/interdisciplinariii/InterdisciplinarIII.java
@@ -0,0 +1,11 @@
+package interdisciplinariii;
+
+public class InterdisciplinarIII {
+
+    public static void main(String[] args) {
+        
+        System.out.println("OLHA A FESTA MACACO, TORCIDA É CORAÇÃO, QUEM NÃO CANTA É AMARGO, NUNCA VAI SAI CAMPEÃO");
+        System.out.println("INTER CAGÃO, INTER CAGÃO");
+    }
+    
+}
